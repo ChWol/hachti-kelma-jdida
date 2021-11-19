fbc91b947bdfd29f5d78da6b8de355d5c331d35a
Christopher Wolters <christopherwolters@MacBook-Pro-von-Christopher.local>
2021-11-19 14:19:25 +0100

    Für Java Script üben

--- src/content/Playground/Playground.js
+++ src/content/Playground/Playground.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from "carbon-components-react";
 
 const Playground = () => {
+    const [number, setNumber] = useState(1);
+
+    function click() {
+        window.alert('test!');
+        setNumber(number+1);
+    }
+
     return (
         <div style={{paddingTop: '50px'}}>
             Your place to learn and do whatever you feel like
             <br/>
             <br/>
-            <Button kind='primary' onClick={() => window.alert('Moufej2a!')}>Try this</Button>
+            <Button kind='primary' onClick={() => click()}>Counter: {number}</Button>
             <ul>
                 <li>
                     Ideen: Ähnliche Wörter vorschlagen bzw. "meinten sie..."
